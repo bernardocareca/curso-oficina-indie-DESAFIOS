a366c219398b7642030db376eb1dd858ce844e84
bernardocareca <uberbernardo12@gmail.com>
2026-02-10 09:56:35 -0300

    Add files via upload

---
+++ Desafio1.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Desafio1 : MonoBehaviour
+{
+    public int x;
+
+
+    public int y;
+
+    string soma = "A soma de ";
+
+    void Start()
+    {
+       Debug.Log("A soma de " + x + " + " + y + " é: " + (x+y));
+
+    }
+    void Update()
+    {
+        
+    }
+} 
